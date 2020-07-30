aa6743ae76d27267eb740983c45ac55c7f66163b
Michał Lubowicz <lubowicz.michal@gmail.com>
2020-07-30 11:22:33 +0200

    Initial Commit

---
+++ Tip Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by Michał Lubowicz on 30/07/2020.
+//  Copyright © 2020 Michał Lubowicz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
